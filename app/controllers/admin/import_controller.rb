846850262cfbf917a85ae8771284a16285330a84
-_- Zaba -_- <bsilva@korewa.com.br>
2012-05-30 10:43:25 -0300

    Comentando Import Controller

--- app/controllers/admin/import_controller.rb
+++ app/controllers/admin/import_controller.rb
@@ -1,13 +1,12 @@
 class Admin::ImportController < ApplicationController
   layout "blank"
   require 'fileutils'
-  require 'faster_csv'  
+#  require 'faster_csv'  
 
   def index
 
   end
-
-
+/
   def imp_sipais
     if File.exists?('public/import/new/SIPAIS.TXT')
       Sipais.delete_all
@@ -94,7 +93,7 @@ class Admin::ImportController < ApplicationController
       @Bairros = Sibai.all
     end
   end  
-  
+  /
 
   
 #  def index
